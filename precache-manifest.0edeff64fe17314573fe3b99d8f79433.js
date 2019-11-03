f43fb477ce0664012a63569d4af591812a8a5ef3
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2019-11-03 12:45:48 -0300

    Updates

---
+++ precache-manifest.0edeff64fe17314573fe3b99d8f79433.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d4d6a8695311975f2adb3b5476a612",
+    "url": "/react-colors-boxes/index.html"
+  },
+  {
+    "revision": "9917e9679fc4c8dd8e49",
+    "url": "/react-colors-boxes/static/css/main.91edc333.chunk.css"
+  },
+  {
+    "revision": "a4868d385b02e9c938d9",
+    "url": "/react-colors-boxes/static/js/2.6b6dc8c3.chunk.js"
+  },
+  {
+    "revision": "9917e9679fc4c8dd8e49",
+    "url": "/react-colors-boxes/static/js/main.87e7154f.chunk.js"
+  },
+  {
+    "revision": "c88b6ae1eee14763efd1",
+    "url": "/react-colors-boxes/static/js/runtime-main.166b4a4f.js"
+  }
+]);
